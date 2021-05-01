46578970692b24f4cd9ec4a8ad9c842006b92a7e
Seafood <410735012@gms.ndhu.edu.tw>
2021-05-01 16:11:03 +0800

    init

---
+++ Assets/Script/Cameracontrol.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameracontrol : MonoBehaviour
+{
+    public float Speed = 2f;
+    public float ASpeed;
+    public float SA = 0.5f;
+
+
+    public Transform CT;
+
+
+    void Update()
+    {
+        
+        float VerD = Mathf.Abs(CT.localPosition.z);
+        float RotD = Mathf.Abs(CT.localRotation.eulerAngles.z);
+
+        Debug.Log(VerD);
+
+        if ((VerD < 2) & (Input.GetAxisRaw("Vertical") != 0))
+        {
+            CT.position += CT.transform.forward * -Input.GetAxisRaw("Vertical") * Speed * Time.deltaTime;
+        }
+        else if ((Input.GetAxis("Mouse X")!=0) & ((RotD < 30) | (RotD > 330)))
+        {
+            CT.Rotate(Vector3.forward * Speed * 0.2f * Input.GetAxis("Mouse X"));
+        }
+        else
+        {
+            CT.position = Vector3.MoveTowards(CT.position, transform.position, Speed * Time.deltaTime);
+            CT.rotation = Quaternion.RotateTowards(CT.rotation, transform.rotation, Speed * 0.15f);
+        }
+
+
+
+    }
+}
---
+++ Assets/Script/CarContral.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarContral : MonoBehaviour
+{
+    public float FSpeed=50f,SSpeed=50f, HSpeed=50f;
+    public float AFSpeed, ASSpeed, AHSpeed;
+    public float FA=2.5f, SA=2f, HA=2f;
+
+    public float LRSpeed = 100f, LRSA = 2f;
+    private Vector2 LInput, ScreenCenter, MouseDistance;
+
+    private float RInput;
+    public float RSpeed = 90f, RA = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LInput.x = Input.GetAxis("Mouse X");
+        LInput.y = Input.GetAxis("Mouse Y");
+        RInput = Mathf.Lerp(RInput, Input.GetAxis("Roll"), RA * Time.deltaTime);
+
+        MouseDistance.x = Mathf.Lerp(MouseDistance.x, LInput.x * LRSpeed, LRSA * Time.deltaTime);
+        MouseDistance.y = Mathf.Lerp(MouseDistance.y, LInput.y * LRSpeed, LRSA * Time.deltaTime);
+
+        MouseDistance = Vector2.ClampMagnitude(MouseDistance, 1f);
+
+        transform.Rotate(-LInput.y * LRSpeed * Time.deltaTime, LInput.x * LRSpeed * Time.deltaTime, -RInput * RSpeed * Time.deltaTime, Space.Self);
+
+        AFSpeed = Mathf.Lerp(AFSpeed, Input.GetAxisRaw("Vertical")*FSpeed, FA*Time.deltaTime);
+        ASSpeed = Mathf.Lerp(ASSpeed, Input.GetAxisRaw("Horizontal") * SSpeed, SA * Time.deltaTime);
+        AHSpeed =  Mathf.Lerp(AHSpeed, Input.GetAxisRaw("Hover") * HSpeed, HA * Time.deltaTime);
+
+
+        transform.position += transform.forward * AFSpeed * Time.deltaTime;
+        transform.position += (transform.right * ASSpeed * Time.deltaTime) + (transform.up * AHSpeed * Time.deltaTime);
+
+
+    }
+}
